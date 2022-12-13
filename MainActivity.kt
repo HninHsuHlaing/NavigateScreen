3bf0999091f01476c52aa271c59fc85c1bd80f58
HninHsuHlaing <58679574+HninHsuHlaing@users.noreply.github.com>
2022-12-13 15:45:52 +0630

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,19 @@
+package com.padcx.navigatescreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this,second_activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
